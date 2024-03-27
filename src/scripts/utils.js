eef94a46cbcdf222de58d04bad36a3a2902ac5b8
Abdul Wahab <abdulwahabse@outlook.com>
2024-03-27 11:50:02 +0100

    Add Vitest unit tests for utils.js

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -1,10 +1,27 @@
+/**
+ * Returns the URL for the given path.
+ * @param {string} path - The path to be converted into a URL.
+ * @returns {string} The URL derived from the given path.
+ */
 function getImageUrl(path) {
     path = '' + path;
     return new URL(path, import.meta.url).href;
 }
 
+/**
+ * Formats the given price into USD currency.
+ * @param {number} price - The price to be formatted.
+ * @throws {Error} If the input is not a number.
+ * @returns {string} The formatted price in USD currency.
+ */
 function formatPrice(price) {
-    const formattedPrice = price.toLocaleString('en-US', {
+    if (typeof price !== 'number') {
+        throw new Error('Invalid input. Expected a number');
+    }
+
+    const input = parseInt(price) < 0 ? 0 : price;
+
+    const formattedPrice = input.toLocaleString('en-US', {
         style: 'currency',
         currency: 'USD',
         minimumFractionDigits: 0,
@@ -12,10 +29,20 @@ function formatPrice(price) {
     return formattedPrice;
 }
 
+/**
+ * Removes the word 'HEADPHONES' from the given string.
+ * @param {string} string - The string from which 'HEADPHONES' will be removed.
+ * @returns {string} The string with 'HEADPHONES' removed.
+ */
 function formatHeading(string) {
     return string.replace('HEADPHONES', '');
 }
 
+/**
+ * Removes the words 'HEADPHONES', 'EARPHONES', and 'SPEAKERS' from the given string.
+ * @param {string} string - The string from which 'HEADPHONES', 'EARPHONES', and 'SPEAKERS' will be removed.
+ * @returns {string} The string with 'HEADPHONES', 'EARPHONES', and 'SPEAKERS' removed.
+ */
 function removeCategory(string) {
     return string
         .replace('HEADPHONES', '')
--- src/scripts/utils.test.js
+++ src/scripts/utils.test.js
@@ -1,11 +1,105 @@
-import { it, expect } from 'vitest';
-import { getImageUrl } from './utils';
+import { it, expect, describe } from 'vitest';
+import {
+    getImageUrl,
+    formatPrice,
+    formatHeading,
+    removeCategory,
+} from './utils';
 
-it('should should return the correct URL', () => {
-    const input = '/images/image.jpg';
-    const expectedResult = new URL(input, import.meta.url).href;
+describe('getImageUrl()', () => {
+    it('should should return the correct URL', () => {
+        const input = '/images/image.jpg';
+        const expectedResult = new URL(input, import.meta.url).href;
 
-    const result = getImageUrl(input);
+        const result = getImageUrl(input);
 
-    expect(result).toBe(expectedResult);
+        expect(result).toBe(expectedResult);
+    });
+
+    it('should convert non-string inputs to strings', () => {
+        const input = 123;
+        const expectedResult = new URL(input, import.meta.url).href;
+
+        const result = getImageUrl(input);
+
+        expect(result).toBe(expectedResult);
+    });
+});
+
+describe('formatPrice()', () => {
+    it('should return the price formatted as USD currency', () => {
+        const input = 1000;
+        const expectedResult = '$1,000';
+
+        const result = formatPrice(input);
+
+        expect(result).toBe(expectedResult);
+    });
+
+    it('should handle negative prices', () => {
+        const input = -1000;
+        const expectedResult = '$0';
+
+        const result = formatPrice(input);
+
+        expect(result).toBe(expectedResult);
+    });
+
+    it('should handle 0 price', () => {
+        const input = 0;
+        const expectedResult = '$0';
+
+        const result = formatPrice(input);
+
+        expect(result).toBe(expectedResult);
+    });
+
+    it('should handle invalid price', () => {
+        const input = 'abc';
+
+        const resultFn = () => formatPrice(input);
+
+        expect(resultFn).toThrow();
+    });
+});
+
+describe('formatHeading()', () => {
+    it('should remove "HEADPHONES" from the string', () => {
+        const input = 'Buy the best HEADPHONES in town!';
+        const expectedResult = 'Buy the best  in town!';
+
+        const result = formatHeading(input);
+
+        expect(result).toBe(expectedResult);
+    });
+
+    it('should handle string without "HEADPHONES"', () => {
+        const input = 'The quick brown fox jumps over the lazy dog.';
+        const expectedResult = input;
+
+        const result = formatHeading(input);
+
+        expect(result).toBe(expectedResult);
+    });
+});
+
+describe('removeCategory()', () => {
+    it('should remove "HEADPHONES", "EARPHONES" and "SPEAKERS" from the string', () => {
+        const input =
+            'Buy the best HEADPHONES, EARPHONES and SPEAKERS in town!';
+        const expectedResult = 'Buy the best ,  and  in town!';
+
+        const result = removeCategory(input);
+
+        expect(result).toBe(expectedResult);
+    });
+
+    it('should handle string without "HEADPHONES", "EARPHONES" and "SPEAKERS"', () => {
+        const input = 'The quick brown fox jumps over the lazy dog.';
+        const expectedResult = input;
+
+        const result = removeCategory(input);
+
+        expect(result).toBe(expectedResult);
+    });
 });
