3bba6618033ded6cbcd391627668934dbbcff3b4
Abdul Wahab <abdulwahabse@outlook.com>
2024-03-31 20:47:57 +0200

    Enhance accessibility by adding unerline in links

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -1,47 +1,26 @@
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Navigation(props) {
-    const activeStyle = {
-        color: '#c35007',
-    };
-
     return (
         <nav className={`navigation ${props.className}`}>
             <ul className="navigation__list">
                 <li className="navigation__item">
-                    <NavLink
-                        exact
-                        to="/"
-                        className="navigation__link"
-                        activeStyle={activeStyle}
-                    >
+                    <NavLink exact to="/" className="navigation__link">
                         Home
                     </NavLink>
                 </li>
                 <li className="navigation__item">
-                    <NavLink
-                        to="/headphones"
-                        className="navigation__link"
-                        activeStyle={activeStyle}
-                    >
+                    <NavLink to="/headphones" className="navigation__link">
                         Headphones
                     </NavLink>
                 </li>
                 <li className="navigation__item">
-                    <NavLink
-                        to="/speakers"
-                        className="navigation__link"
-                        activeStyle={activeStyle}
-                    >
+                    <NavLink to="/speakers" className="navigation__link">
                         Speakers
                     </NavLink>
                 </li>
                 <li className="navigation__item">
-                    <NavLink
-                        to="/earphones"
-                        className="navigation__link"
-                        activeStyle={activeStyle}
-                    >
+                    <NavLink to="/earphones" className="navigation__link">
                         Earphones
                     </NavLink>
                 </li>
