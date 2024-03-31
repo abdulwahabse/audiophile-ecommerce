678a7a9a41498777b8a0ad109dd6cf29db0745e4
Abdul Wahab <abdulwahabse@outlook.com>
2024-03-31 14:42:59 +0200

    Enhance accessibility for Categories

--- src/components/common/Categories.jsx
+++ src/components/common/Categories.jsx
@@ -8,87 +8,96 @@ export default function Categories(props) {
     const arrowIcon = getImageUrl('icons/icon-arrow-right.svg');
 
     return (
-        <div className={`categories ${props.className}`}>
-            <div className="container">
+        <nav
+            aria-label="Product Categories"
+            className={`categories ${props.className}`}
+        >
+            <ul className="container">
                 {/* headphones category */}
-                <Link
-                    to="/headphones"
-                    className="categories__link categories__link--1"
-                >
-                    <div className="categories__category">
-                        <img
-                            className="categories__image"
-                            src={headphonesImg}
-                            alt="headphones"
-                        />
-                        <h2 className="categories__heading heading-small">
-                            Headphones
-                        </h2>
-                        <div className="categories__cta">
-                            <p className="categories__cta-text categories__cta-text--1">
-                                Shop
-                            </p>
+                <li className="categories__li">
+                    <Link
+                        to="/headphones"
+                        className="categories__link categories__link--1"
+                    >
+                        <div className="categories__category">
                             <img
-                                className="categories__cta-icon categories__cta-icon--1"
-                                src={arrowIcon}
-                                alt="arrow right"
+                                className="categories__image"
+                                src={headphonesImg}
+                                alt="headphones"
                             />
+                            <h2 className="categories__heading heading-small">
+                                Headphones
+                            </h2>
+                            <div className="categories__cta">
+                                <p className="categories__cta-text categories__cta-text--1">
+                                    Shop
+                                </p>
+                                <img
+                                    className="categories__cta-icon categories__cta-icon--1"
+                                    src={arrowIcon}
+                                    alt="arrow right"
+                                />
+                            </div>
                         </div>
-                    </div>
-                </Link>
+                    </Link>
+                </li>
                 {/* speakers category */}
-                <Link
-                    to="/speakers"
-                    className="categories__link categories__link--2"
-                >
-                    <div className="categories__category">
-                        <img
-                            className="categories__image"
-                            src={speakersImg}
-                            alt="headphones"
-                        />
-                        <h2 className="categories__heading heading-small">
-                            Speakers
-                        </h2>
-                        <div className="categories__cta">
-                            <p className="categories__cta-text categories__cta-text--2">
-                                Shop
-                            </p>
+                <li className="categories__li">
+                    <Link
+                        to="/speakers"
+                        className="categories__link categories__link--2"
+                    >
+                        <div className="categories__category">
                             <img
-                                className="categories__cta-icon categories__cta-icon--2"
-                                src={arrowIcon}
-                                alt="arrow right"
+                                className="categories__image"
+                                src={speakersImg}
+                                alt="headphones"
                             />
+                            <h2 className="categories__heading heading-small">
+                                Speakers
+                            </h2>
+                            <div className="categories__cta">
+                                <p className="categories__cta-text categories__cta-text--2">
+                                    Shop
+                                </p>
+                                <img
+                                    className="categories__cta-icon categories__cta-icon--2"
+                                    src={arrowIcon}
+                                    alt="arrow right"
+                                />
+                            </div>
                         </div>
-                    </div>
-                </Link>
+                    </Link>
+                </li>
                 {/* earphones category */}
-                <Link
-                    to="/earphones"
-                    className="categories__link categories__link--1"
-                >
-                    <div className="categories__category">
-                        <img
-                            className="categories__image categories__image--3"
-                            src={earphonesImg}
-                            alt="headphones"
-                        />
-                        <h2 className="categories__heading heading-small">
-                            Earphones
-                        </h2>
-                        <div className="categories__cta">
-                            <p className="categories__cta-text categories__cta-text--3">
-                                Shop
-                            </p>
+                <li className="categories__li">
+                    <Link
+                        to="/earphones"
+                        className="categories__link categories__link--1"
+                    >
+                        <div className="categories__category">
                             <img
-                                className="categories__cta-icon categories__cta-icon--3"
-                                src={arrowIcon}
-                                alt="arrow right"
+                                className="categories__image categories__image--3"
+                                src={earphonesImg}
+                                alt="headphones"
                             />
+                            <h2 className="categories__heading heading-small">
+                                Earphones
+                            </h2>
+                            <div className="categories__cta">
+                                <p className="categories__cta-text categories__cta-text--3">
+                                    Shop
+                                </p>
+                                <img
+                                    className="categories__cta-icon categories__cta-icon--3"
+                                    src={arrowIcon}
+                                    alt="arrow right"
+                                />
+                            </div>
                         </div>
-                    </div>
-                </Link>
-            </div>
-        </div>
+                    </Link>
+                </li>
+            </ul>
+        </nav>
     );
 }
